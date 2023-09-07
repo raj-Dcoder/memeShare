6d6719aa6326a7c72dbd311ca78ff18829dc382c
Rajveer <rajveersinghranaofficial@gmail.com>
2023-09-07 16:42:45 +0530

    create API call

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.android.gms:play-services-cronet:18.0.1")
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
+
 }
--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,13 +1,46 @@
 package com.example.memeshare
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
+
+    }
+    private fun loadMeme(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.com/gimme"
+
+        val memeImageView: ImageView = findViewById(R.id.memeImageView)
+
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            { response ->
+                val url=response.getString("url")
+                Glide.with(this).load(url).into(memeImageView)
+
+            },
+            {
+                Toast.makeText(this,"something went wrong",Toast.LENGTH_LONG).show()
+            })
+
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
     }
 
     fun shareMeme(view: View) {
